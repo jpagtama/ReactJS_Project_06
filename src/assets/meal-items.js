ef79021cfe9ce2cf5880845c5da418f44627b243
Julian Pagtama <jpagtama@gmail.com>
2022-05-25 15:21:14 -0700

    Adds meal-items placeholder array

---
+++ src/assets/meal-items.js
@@ -0,0 +1,26 @@
+export const mealItems = [
+    {
+        id: 1,
+        title:"Sushi",
+        desc:"Finest fish and veggies",
+        price:22.99,
+    },
+    {
+        id: 2,
+        title:"Schnitzel",
+        desc:"A German specialty!",
+        price:16.50,
+    },
+    {
+        id: 3,
+        title:"Barbeque Burger",
+        desc:"American, raw, meaty",
+        price:12.99,
+    },
+    {
+        id: 4,
+        title:"Green Bowl",
+        desc:"Healthy... and green",
+        price:18.99
+    },
+]
