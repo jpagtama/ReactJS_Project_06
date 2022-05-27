d9c33c75141f83be585e9657bbb8dbc6d4840165
Julian Pagtama <jpagtama@gmail.com>
2022-05-27 16:02:54 -0700

    Portals the Modal to have semantic placement

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import styles from './Modal.module.css'
 import Card from './Card'
 import BackDrop from './BackDrop'
 
 const Modal = props => {
-    return (
-        <React.Fragment>
-            <Card className={styles.modal} >
-                {props.children}
-            </Card>
-            <BackDrop />
-        </React.Fragment>
-    )
+    const ModalAlert = () => {
+        return (
+            <React.Fragment>
+                <Card className={styles.modal} >
+                    {props.children}
+                </Card>
+                <BackDrop />
+            </React.Fragment>
+        )
+    }
+
+    return ReactDOM.createPortal(<ModalAlert />, document.getElementById("modal-root"))
 }
 export default Modal
